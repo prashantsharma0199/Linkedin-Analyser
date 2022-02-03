a4427ecf9044bf83471f680b565463db7ba25277
Prashant Sharma <47805191+prashantsharma0199@users.noreply.github.com>
2022-02-03 20:16:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, send_file
+import senti
+import senti_analysis
+
+app= Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        usern = (request.form['username'])
+        pswd = (request.form['password'])
+        usery = (request.form['user'])
+        filey = (request.form['filename'])
+        senti.findData(usery, filey, usern, pswd)
+        senti_analysis.analysis(usery, filey)
+        return render_template('result.html', prediction=usery)
+
+@app.route('/download', methods=['POST'])
+def download():
+    if request.method == 'POST':
+	    filename= (request.form['us'])
+	    return send_file('static\\report\\'+filename+'\\Report-'+filename+'.txt', as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
